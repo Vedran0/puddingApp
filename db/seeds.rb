a36764bc5928357a865a680b5bfbdbc2c9588442
Vedran Mijatović <vedranmijatovic0@gmail.com>
2017-09-16 16:32:51 +0200

    deploy problems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,7 @@
-# Pudding.create(name: "Vedran", email: "vedranmijatovic0@gmail.com", password: "super0800")
-# Pudding.create(name: "Elizabeta", email: "el.antunovic0@gmail.com", password: "super0800")
-# puts "Pudding created"
+Pudding.create(name: "Vedran", email: "vedranmijatovic0@gmail.com", password: "super0800")
+Pudding.create(name: "Elizabeta", email: "el.antunovic0@gmail.com", password: "super0800")
+puts "Pudding created"
 
-# 5.times do |index|
-#   Place.create(name: "Place #{index}")
-# end
-
-# Pudding.all.each do |pudding|
-#   Setting.create(pudding_id: pudding.id)
-# end
+Pudding.all.each do |pudding|
+  Setting.create(pudding_id: pudding.id)
+end
