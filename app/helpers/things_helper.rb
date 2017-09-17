e9739a1759b2051080441f77ad47fbbfc5c97458
Vedran Mijatović <vedranmijatovic0@gmail.com>
2017-09-18 00:22:55 +0200

    add more backgrounds and style things

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -1,2 +1,33 @@
 module ThingsHelper
+
+  def achievement_heaviness_text(achievement_heaviness)
+    case achievement_heaviness
+    when 1
+      "Very Easy"
+    when 2
+      "Easy"
+    when 3
+      "We Will Do It"
+    when 4
+      "We CAN Do it"
+    when 5
+      "One Day"
+    end
+  end
+
+    def achievement_heaviness_color(achievement_heaviness)
+    case achievement_heaviness
+    when 1
+      "#388e3c"
+    when 2
+      "#1976d2"
+    when 3
+      "#fbc02d"
+    when 4
+      "#f57c00"
+    when 5
+      "#c62828"
+    end
+  end
+
 end
