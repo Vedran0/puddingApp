c3c4c763f46d1b44258669d1fc7f009b66d1eb9a
Vedran Mijatović <vedranmijatovic0@gmail.com>
2017-11-19 18:32:11 +0100

    fix time_travel date pick problem

--- app/controllers/song_of_the_days_controller.rb
+++ app/controllers/song_of_the_days_controller.rb
@@ -12,9 +12,10 @@ class SongOfTheDaysController < ApplicationController
   private
 
   def set_page
+    binding.pry
     page = 1
     page = params[:page]
-    page = SongOfTheDay.find_by(of_the_day: params[:date_picked]).page if params[:date_picked]
+    page = SongOfTheDay.find_by(of_the_day: params[:date_picked].to_date).page if params[:date_picked]
     page
   end
 
