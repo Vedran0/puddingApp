9e8286d39f7fc4af143b7682867c879140f80b44
Vedran Mijatović <vedranmijatovic0@gmail.com>
2017-11-23 20:47:29 +0100

    change quotes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,34 +4,18 @@ class ApplicationController < ActionController::Base
   before_action :set_pudding_quote, if: :devise_controller?
 
   def set_pudding_quote
-    unless params[:hint_step]
-      @random_quote = get_default_quotes.sample()
-    else
-      params[:hint_step] = params[:hint_step].to_i>5 ? 4 : params[:hint_step].to_i+1
-      @random_quote = get_hint[params[:hint_step].to_i-1]
-    end
-    if params[:pudding] && params[:pudding][:name].chomp.downcase == "beta"
-      @random_quote = "Your name, not your nickname!"
-      params[:hint_step] = 0
-    elsif params[:pudding] && params[:pudding][:name] == "elizabeta"
-      @random_quote = "Capital \"E\" would be nice..."
-      params[:hint_step] = 0
-    end
+    @random_quote = get_default_quotes.sample()
   end
 
   def get_default_quotes
-    quotes = ["The soft heart is a heart made of pudding!",
-              "A beautiful pudding delights the eye; a wise pudding, the understanding; a pure one, the soul.",
-              "He was a wise man who invented pudding.",
-              "A room without puddings is like a body without a soul.",
-              "So many puddings, so little time.",
-              "Take into account that great love and great achievements involve great pudding.",
-              "In the end, it’s not the years in your life that count. It’s the number of puddings you ate.",
-              "Without pudding, life would be a mistake.",
-              "We accept the pudding we think we deserve.",
-              "Good friends, good pudding, and a sleepy conscience: this is the ideal life.",
-              "We are all in the gutter, but some of us are looking at the pudding.",
-              "The man who does not eat pudding has no advantage over the man who cannot eat pudding."
+    quotes = ["A good pudding is a continual Christmas.",
+              "Christmas, my pudding, is love in action.",
+              "Christmas is a pudding stuffed with sugary goodness.",
+              "My idea of Christmas, whether old-fashioned or modern, is very simple: pudding others.",
+              "Christmas waves a magic wand over the world, and behold, everything is puddingy and more beautiful.",
+              "He who has not Christmas in his pudding heart will never find it under a tree.",
+              "Teacher says every time a bell rings, an angel gets his pudding.",
+              "Blessed is the season which engages the whole world in a conspiracy of love and pudding."
                  ]
   end
 
