2ba6d34b8ba791848452086ecaa9306dd66488f8
Vedran Mijatovic <vedranmijatovic0@gmail.com>
2018-09-09 17:58:26 +0200

    fix places and finish jukebox

--- app/controllers/visited_places_controller.rb
+++ app/controllers/visited_places_controller.rb
@@ -1,9 +1,11 @@
 class VisitedPlacesController < ApplicationController
   before_action :set_visited_place, only: [:show, :edit, :update, :destroy]
+  before_action :set_about_page, only: :index
 
   # GET /visited_places
   def index
     @visited_places = VisitedPlace.all.order(visited_at: :asc)
+    @region = params[:region] || "world"
   end
 
   # GET /visited_places/new
@@ -41,5 +43,11 @@ class VisitedPlacesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def visited_place_params
       params.require(:visited_place).permit(:name, :visited_at, :place_type)
+    end
+
+    def set_about_page
+      @about_page = "<p>Wanna travel?</p>
+                      <p>The world is ours!</p>
+                      <p>Conquer the world!</p>"
     end
 end
--- app/models/visited_place.rb
+++ app/models/visited_place.rb
@@ -1,2 +1,3 @@
 class VisitedPlace < ActiveRecord::Base
+
 end
