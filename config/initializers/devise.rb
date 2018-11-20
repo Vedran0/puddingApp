21f7a286b326a2442afd20d9511ae11535b2cfa8
Vedran Mijatovic <vedranmijatovic0@gmail.com>
2018-11-20 11:24:09 +0100

    remove precompiled assets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = '27506f09835d7c99abdb694053dceed537fa761f7bcb99673473db65379f980edd72ef0c30049d16763373e8ff5b72b2a9bd55d175fc47d768aabe806ec31881'
+  # config.secret_key = '27506f09835d7c99abdb694053dceed537fa761f7bcb99673473db65379f980edd72ef0c30049d16763373e8ff5b72b2a9bd55d175fc47d768aabe806ec31881'
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
