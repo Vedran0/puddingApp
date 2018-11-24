2d1028a2935c26547f84f3ef2d1d72bed353a4b0
Vedran Mijatovic <vedranmijatovic0@gmail.com>
2018-11-24 20:33:49 +0100

    fix santas helper

--- app/helpers/santas_helper.rb
+++ app/helpers/santas_helper.rb
@@ -1,7 +1,7 @@
 module SantasHelper
 
 	def new_calendar_days
-		new_days = Date.today.day-current_pudding.calendar_number.numbers_drawn.size
+		new_days = Date.today.day-current_pudding.count_numbers_drawn
 		"Open your #{new_days} new calendar #{new_days > 1 ? "days" : "day"}"
 	end
 
